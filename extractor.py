1ec33bbca3609c8c928a1c0a92e3170957fccb69
孙娇 <sunjiao@sunjiao.moe>
2021-08-03 12:45:18 +0800

    add properties statistics

--- extractor.py
+++ extractor.py
@@ -6,6 +6,7 @@ from Bio.Seq import Seq
 
 from Feature import Feature
 from statistictor import write_out_file
+from Bio.SeqUtils import GC
 
 
 def extract(filename: str, contain: list = None, exceptl: list = None):
@@ -25,17 +26,25 @@ def extract(filename: str, contain: list = None, exceptl: list = None):
     with open(filename) as file:
         seq_dict: dict = {}  # key为序列名称，value为另一个字典，子字典的key为注释名称，value为出现次数
         # keys are sequence name, values are child dictionaries
-        # keys of hild dictionaries are annotation name, values are occurrence times
+        # keys of child dictionaries are annotation name, values are occurrence times
         anno_list: list[str] = []  # 注释名列表，内容为注释名称 items are all annotations
+        info_dict: dict = {}  # key为序列名称，value为另一个字典，子字典的key为序列属性，value为属性值
+        # keys are sequence name, values are child dictionaries, which contains other properties and values
+        info_list: list[str] = ['Length', 'LSC', 'SSC', 'IR', 'GC-content', 'PCG', 'tRNA', 'rRNA']
 
         for record in GenBank.parse(file):
             processed = []
             # 储存已处理过的feature，用于判断拷贝数，每条序列开始时重置。
             # processed feature, used for copy amount, reset when new sequence starts
 
-            seq = (record.organism + '-' + record.accession[0]).replace(' ', '_')
-            if seq not in seq_dict:
-                seq_dict[seq] = {}
+            seq_name = (record.organism + '-' + record.accession[0]).replace(' ', '_')
+            if seq_name not in seq_dict:
+                seq_dict[seq_name] = {}
+            if seq_name not in info_dict:
+                info_dict[seq_name] = {'Length': '', 'LSC': '', 'SSC': '', 'IR': '', 'GC-content': '',
+                                       'PCG': 0, 'tRNA': 0, 'rRNA': 0}
+            info_dict[seq_name]['Length'] = len(record.sequence)
+            info_dict[seq_name]['GC-content'] = GC(record.sequence)
 
             for feature in record.features:
                 # 遇到list为空也会算作not in contain，所以应该先检查contain非空。
@@ -65,10 +74,10 @@ def extract(filename: str, contain: list = None, exceptl: list = None):
 
                     # 该注释在该序列中出现的次数
                     # occurrence times of this annotation in this sequence
-                    if fullname not in seq_dict[seq]:
-                        seq_dict[seq][fullname] = 1
+                    if fullname not in seq_dict[seq_name]:
+                        seq_dict[seq_name][fullname] = 1
                     else:
-                        seq_dict[seq][fullname] += 1
+                        seq_dict[seq_name][fullname] += 1
 
                     suffix = ''  # 后缀默认为空字符串
                     # print(record.organism + ' ' + record.accession[0] + ' ' + fullname + ' ', end=' ')
@@ -95,20 +104,39 @@ def extract(filename: str, contain: list = None, exceptl: list = None):
                     #   |--tRNA
                     #   |--others
 
-                    outname = root + '\\' + myfeature.key + '\\' + myfeature.qualifier_dict[name_key] + suffix + '.fasta'
+                    outname = root + '\\' + myfeature.key + '\\' + myfeature.qualifier_dict[
+                        name_key] + suffix + '.fasta'
                     if not os.path.exists(root + '\\' + myfeature.key):
                         os.mkdir(root + '\\' + myfeature.key)
 
                     with open(outname, 'a', newline='') as outfile:  # 'w': write,写入并覆盖原有内容；‘a': append, 附加在原有内容之后。
-                        outfile.write(f'>{seq}\n')
+                        outfile.write(f'>{seq_name}\n')
+                        seq_of_it = ''
                         for interval in myfeature.intervals:
                             if interval.complement:
-                                outfile.write(
-                                    # 计算已知序列的反向互补序列 reverse complement of the sequence
-                                    str(Seq(record.sequence[interval.start - 1:interval.end]).reverse_complement()))
+                                # 计算已知序列的反向互补序列 reverse complement of the sequence
+                                seq_of_it = seq_of_it + str(
+                                    Seq(record.sequence[interval.start - 1:interval.end]).reverse_complement())
                             else:
-                                outfile.write(record.sequence[interval.start - 1:interval.end])
-                        outfile.write('\n')
+                                seq_of_it = seq_of_it + record.sequence[interval.start - 1:interval.end]
+
+                        if 'LSC' in outname:
+                            info_dict[seq_name]['LSC'] = len(seq_of_it)
+                        elif 'SSC' in outname:
+                            info_dict[seq_name]['SSC'] = len(seq_of_it)
+                        elif 'IR' in outname or 'repeat_region' in outname:
+                            if info_dict[seq_name]['IR'] == '':
+                                info_dict[seq_name]['IR'] = len(seq_of_it)
+                            elif info_dict[seq_name]['IR'] != len(seq_of_it):
+                                print('repeat region length difference, please check it')
+                        elif 'CDS' in outname:
+                            info_dict[seq_name]['PCG'] = info_dict[seq_name]['PCG'] + 1
+                        elif 'tRNA' in outname:
+                            info_dict[seq_name]['tRNA'] = info_dict[seq_name]['tRNA'] + 1
+                        elif 'rRNA' in outname:
+                            info_dict[seq_name]['rRNA'] = info_dict[seq_name]['rRNA'] + 1
+
+                        outfile.write(f'{seq_of_it}\n')
 
                     processed.append(fullname + suffix)
 
@@ -117,13 +145,17 @@ def extract(filename: str, contain: list = None, exceptl: list = None):
                 except IndexError as e:
                     print(str(e) + record.organism + ' ' + record.accession[0] + '\n' + str(feature))
 
-        print('Writing csv file')
+        print('Writing statistic csv file')
         outname: str = root + '/annotation_statistic.csv'
-
         write_out_file(outname, anno_list, seq_dict)
+
+        print('Checking properties and writing to csv file')
+        p_outname: str = root + '/properties_statistic.csv'
+        write_out_file(p_outname, info_list, info_dict)
+
         print('Annotation extraction complete.')
 
 
 if __name__ == '__main__':
-    extract("input.gb", contain=[# 'CDS', 'gene', 'tRNA', 'rRNA'
-                                 ], exceptl=[])
+    extract("input.gb", contain=[  # 'CDS', 'gene', 'tRNA', 'rRNA'
+    ], exceptl=[])
